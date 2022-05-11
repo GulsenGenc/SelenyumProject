c2350a4cb45d5abe461159d84f356e3de9f067a1
Gulsen Genc <gulsengenctuncer@gmail>
2022-05-11 23:48:41 +0300

    11.05.2022

---
+++ src/test/java/Homework/Homework02.java
@@ -0,0 +1,38 @@
+package Homework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Homework02 {
+    /*
+    Tekrar Testi
+1. Bir class oluşturun : AmazonSearchTest
+2. Main method oluşturun ve aşağıdaki görevi tamamlayın.
+a.google web sayfasına gidin. https://www. amazon.com/
+b. Search(ara) “city bike”
+c. Amazon'da görüntülenen ilgili sonuçların sayısını yazdırın
+d. “Shopping” e tıklayın.
+e. Sonra karşınıza çıkan ilk sonucun resmine tıklayın.
+     */
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","resources/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //a.google web sayfasına gidin. https://www. amazon.com/
+        driver.get("https://www. amazon.com/");
+        //b. Search(ara) “city bike”
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("city bike");
+        aramaKutusu.click();
+
+        //c. Amazon'da görüntülenen ilgili sonuçların sayısını yazdırın
+        //d. “Shopping” e tıklayın.
+        //e. Sonra karşınıza çıkan ilk sonucun resmine tıklayın.
+    }
+}
